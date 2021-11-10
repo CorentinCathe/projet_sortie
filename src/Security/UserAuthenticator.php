dc6d41e5ec7bdbdf12b48a63a8a1e6c0c5d346ab
cocat <co.cathelinais@gmail.com>
2021-11-10 09:32:32 +0100

    finish login page

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -51,8 +51,7 @@ class UserAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('main'));
     }
 
     protected function getLoginUrl(Request $request): string
