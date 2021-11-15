50a73b56d1212fe6777651f0e4441b960f77ae76
Sacadoche <nicolasfauveau4@gmail.com>
2021-11-15 10:55:15 +0100

    Fix mainController wrong use of Request and unimported class

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,9 +5,10 @@ namespace App\Controller;
 use App\Data\SearchData;
 use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\SearchSortieForm;
 
 class MainController extends AbstractController
 {
