7d9af13f39f9bc828d32bd67f9748cbb5f23e92f
Sacadoche <nicolasfauveau4@gmail.com>
2021-11-15 10:51:59 +0100

    Fix mainController with sortie index filters

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Data\SearchData;
 use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,10 +14,16 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="main")
      */
-    public function index(SortieRepository $sortieRepository): Response
+    public function index(SortieRepository $sortieRepository, Request $request): Response
     {
+        $data = new SearchData();
+        $form = $this->createForm(SearchSortieForm::class,$data);
+        $form->handleRequest($request);
+        $user = $this->getUser();
         return $this->render('sortie/index.html.twig', [
-            'sorties' => $sortieRepository->findAll(),
+            'sorties' => $sortieRepository->findSearch($data, $user),
+            'user' => $user,
+            'form' => $form->createView()
         ]);
     }
 }
